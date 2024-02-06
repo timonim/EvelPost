5f12019bd4c674628fc8e966d9d44f804a950222
Tim <portnoytim@gmail.com>
2024-02-06 18:54:37 +0200

    mobile mode and general tweaks and fixes, slider adjustments, responsive tweaks

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const nickNameLabel = document.querySelector("label[for='nick-name']");
 const toggle = document.getElementById('show-nick-name');
 
 function updateNicknameVisibility() {
-  if(toggle.checked) {
+  if (toggle.checked) {
     nickNameField.style.display = 'block';
     nickNameLabel.style.display = 'block';
   } else {
@@ -56,8 +56,11 @@ function updatePreview() {
     const lastNameElement = namePreviewElement.querySelector('#last-name');
     lastNameElement.textContent = values['last-name'];
 
+    const minFontSize = 0.5; // minimum font size in em
+    const maxFontSize = 30; // maximum font size in em
+    
     // Update font size based on slider
-    const fontSize = `${textSizeSlider.value}vw`;
+    const fontSize = `clamp(${minFontSize}em, ${textSizeSlider.value}vw, ${maxFontSize}em)`;
     namePreviewElement.style.fontSize = fontSize;
 
     // Update obituary text with formatted date and day of week
@@ -96,7 +99,7 @@ function updatePreview() {
 
     // Update preview with Hebrew formatted date
     const showHebrewDate = document.getElementById('show-hebrew-date').checked;
-    
+
     // Obituary text
     const obituaryText = `
       <div id="intro">
@@ -127,61 +130,93 @@ updatePreview();
 form.addEventListener('input', updatePreview);
 textSizeSlider.addEventListener('input', updatePreview);
 
-// Export to PDF feature
-function exportToPDF() {
-  // Open a new tab
-  const newTab = window.open('', '_blank');
+async function exportToPDF() {
+  try {
+    // Open a new tab
+    const newTab = window.open('', '_blank');
 
-  // Generate the HTML content for printing
-  const htmlContent = `
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    // Generate the HTML content for printing
+    const htmlContent = `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <link rel="preconnect" href="https://fonts.googleapis.com">
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+          <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Hebrew:wdth,wght@62.5..100,100..900&display=swap" rel="stylesheet">
+          <title>Obituary Preview</title>
+          <link rel="stylesheet" href="styles.css">
+          <style>
 
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Hebrew:wdth,wght@62.5..100,100..900&display=swap" rel="stylesheet">
-        
-        
-        <title>Obituary Preview</title>
-        <link rel="stylesheet" href="styles.css">
-        <style>
-          @page {
-            size: A4 landscape;
-          }
-          body {
-            margin: 0;
-            padding: 4% 2% 2% 2%;
-          }
-          #preview-content {
-            margin: 0;
-            background-color: white;
-            aspect-ratio: 297 / 210;
-            zoom: 1.8; /* Adjust the zoom factor as needed */
-                    }
-        </style>
-      </head>
-      <body>
-        <div id="preview-content">
-          ${previewElement.innerHTML}
-        </div>
-      </body>
-    </html>
-  `;
+              @page {
+                size: 29.7cm 21cm;
+                margin: 0;
+                size: a4 landscape;
+              }
 
-  // Write the HTML content to the new tab
-  newTab.document.open();
-  newTab.document.write(htmlContent);
-  newTab.document.close();
+              header,
+              footer {
+                display: none:
+              }
 
-  // Wait for the new tab to finish loading
-  newTab.onload = function() {
+            body {
+              margin: 0;
+              // padding: 4% 2% 2% 2%;
+              background-color: white;
+              width: 29.7cm;
+              height: 21cm;
+            }
+
+            #preview-content {
+              margin: 4%; /* Center the content horizontally */
+              background-color: white;
+              aspect-ratio: 297 / 210;
+              // zoom: 1.8;
+              font-size: 2.8vw;
+              border: 0.8em solid #000000;
+
+
+              -webkit-text-size-adjust: none;
+              -moz-transform: scale(1.8);
+              -moz-transform-origin: 0 0;
+              overflow: hidden;
+            }
+
+            name-preview {
+              transform: scale(2.1);
+            }
+
+          </style>
+        </head>
+        <body>
+          <div id="preview-content">
+            ${previewElement.innerHTML}
+          </div>
+        </body>
+      </html>
+    `;
+
+    // Write the HTML content to the new tab
+    newTab.document.open();
+    newTab.document.write(htmlContent);
+    newTab.document.close();
+
+    // Wait for the new tab to finish loading
+    await new Promise((resolve) => {
+      newTab.onload = resolve;
+    });
+
     // Print the new tab
     newTab.print();
 
     // Close the new tab after printing
-    newTab.close();
-  };
+    if (navigator.userAgent.includes("Android")) {
+      window.close();
+    } else {
+      newTab.close();
+    }
+  } catch (error) {
+    console.error('An error occurred while exporting to PDF:', error);
+  }
 }
